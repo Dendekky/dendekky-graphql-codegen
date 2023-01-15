6b9293377beb39e8023c55b2178f88b2e6bd6d32
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2023-01-15 23:50:51 +0100

    feat: allow authorization for protected apis

--- pages/api/codegen.ts
+++ pages/api/codegen.ts
@@ -1,38 +1,47 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { codegen } from '@graphql-codegen/core';
-import fs from 'fs';
-import path from 'path';
-import { buildSchema, printSchema, parse, GraphQLSchema } from 'graphql';
+// import { generate } from '@graphql-codegen/cli'
+import { printSchema, parse, GraphQLSchema } from 'graphql';
 import * as typescriptPlugin from '@graphql-codegen/typescript';
+import { loadSchema } from '@graphql-tools/load';
+import { UrlLoader } from '@graphql-tools/url-loader';
 
-const schema: GraphQLSchema = buildSchema(`type A { name: String }`);
-const outputFile = 'filename.ts';
-const config = {
+const generateConfig = (schema: GraphQLSchema) => ({
   documents: [],
   config: {},
   // used by a plugin internally, although the 'typescript' plugin currently
   // returns the string output, rather than writing to a file
-  filename: outputFile,
+  filename: '',
   schema: parse(printSchema(schema)),
   plugins: [
-    // Each plugin should be an object
     {
-      typescript: {}, // Here you can pass configuration to the plugin
+      typescript: {},
     },
   ],
   pluginMap: {
     typescript: typescriptPlugin,
   },
-};
+});
 
 const webCodegen = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  let schemaOptions: any = {
+    loaders: [new UrlLoader()],
+  };
+  const { authorization } = req.body;
+  const graphqlApiEndpoint =
+    req.body.graphqlApiEndpoint || req.query.graphqlApiEndpoint;
+
+  if (authorization) {
+    schemaOptions = {
+      ...schemaOptions,
+      headers: { authorization },
+    };
+  }
+  const schema = await loadSchema(graphqlApiEndpoint, schemaOptions);
+  const config = generateConfig(schema);
   const output = await codegen(config);
-  //   await fs.writeFileSync(path.join(__dirname, outputFile), output, 'utf8');
-//   console.log('Outputs generated!', output, 'done');
   const formattedStringOutput = output.replaceAll('\n', '');
-  res
-    .status(200)
-    .json({ data: formattedStringOutput });
+  return res.status(200).json({ data: formattedStringOutput });
 };
 
 export default webCodegen;
