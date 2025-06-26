364afeeca73275305c118cf4e93aac74628d7c02
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2025-06-26 17:18:54 +0100

    refactor: simplify ThemeProvider component by removing redundant type definition and streamlining props usage

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,19 +1,8 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
-type ThemeProviderProps = {
-  children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
-}
-
-export function ThemeProvider({
-  children,
-  ...props
-}: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
